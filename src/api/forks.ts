acfb04abe7c14fe99d91e22b203217b340449eaf
wenxig <wenxinguo12@yeah.net>
2026-07-19 23:29:51 +0800

    feat(forks): [pub] 添加分流

--- src/api/forks.ts
+++ src/api/forks.ts
@@ -1,11 +1,9 @@
 export const image = [
-  'https://api.macintosh-serve.org/media',
-  'https://api.cosfl-airapi.org/media',
+  'https://api.cos-dkop.net/media',
   'https://api.cosavtiktok.club/media'
 ]
 export const apiGetter = []
 export const api = [
-  'https://api.macintosh-serve.org/api/app/v1',
-  'https://api.cosfl-airapi.org/api/app/v1',
+  'https://api.cos-dkop.net/api/app/v1',
   'https://api.cosavtiktok.club/api/app/v1'
 ]
